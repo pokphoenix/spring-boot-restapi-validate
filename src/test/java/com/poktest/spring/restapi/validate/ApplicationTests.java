083e637702f98decd339b2edb9254e969a79eaa9
komsan.kr(pok) <pokphoenix@gmail.com>
2019-09-26 17:35:27 +0700

    @add(pok) -add test with service

--- src/test/java/com/poktest/spring/restapi/validate/ApplicationTests.java
+++ src/test/java/com/poktest/spring/restapi/validate/ApplicationTests.java
@@ -1,7 +1,12 @@
 package com.poktest.spring.restapi.validate;
 
+import com.poktest.spring.restapi.validate.service.BookService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class ApplicationTests {
@@ -9,5 +14,15 @@ class ApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	@Autowired
+	BookService bookService ;
+
+	@DisplayName("Test Spring @Autowired Integration")
+	@Test
+	void testGet() {
+		assertEquals(3,bookService.get().size());
+	}
+
 
 }
